40e899fe8894396a9b15490baeef7276532396dc
Fino <finoraf01@gmail.com>
2025-06-01 15:08:25 +0700

    redirect message pesanan ke wa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -74,9 +74,18 @@ export default function Home() {
                       <p key={index}>{line}</p>
                     ))}
                   </div>
-                  <button className="border border-red-500 text-red-500 px-4 py-2 rounded hover:bg-red-500 hover:text-white transition">
+                  <a
+                    href={`https://wa.me/628112890087?text=${encodeURIComponent(
+                      `Halo, saya ingin memesan produk berikut:\n\n` +
+                      `Nama Produk: ${product.title} | ${product.subtitle}\n` +
+                      `Deskripsi Produk: `
+                    )}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block border border-red-500 text-red-500 px-4 py-2 rounded hover:bg-red-500 hover:text-white transition"
+                  >
                     Pesan Sekarang
-                  </button>
+                  </a>
                 </div>
 
                 {/* Kanan: Gambar */}
